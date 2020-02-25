c3254cbfb342ff5d571f0c3a74cb0de061a2ecea
timdeklijn@gmail.com <200zes1987>
2020-02-25 08:41:20 +0100

    Create subplots of random augmentation

--- tst.py
+++ tst.py
@@ -1,8 +1,15 @@
 from PIL import Image as pil_image
 import im_augment as aug
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     print("[*] Image augmentation scripts")
     img = pil_image.open("parrots.jpg")
-    img = aug.change_sharpness(img, factor=1.8)
-    aug.show_image(img)
+
+    fig, axes = plt.subplots(5, 5, figsize=(10, 10))
+    for i in range(5):
+        for j in range(5):
+            axes[i, j].imshow(aug.random_augment(img))
+            axes[i, j].axis("off")
+    plt.tight_layout()
+    plt.show()
